386331cdc867f585cb10b0e5a09c59077f63df8f
AlinaKovtun <alinakovtun11@gmail.com>
2018-04-20 17:29:09 +0300

    7file
    count lines

--- HW 7/7.py
+++ HW 7/7.py
@@ -4,3 +4,14 @@
 #       - кількість унікальних слів
 #       - кількість голосних та приголосних букв
 #Зверніть увагу що в тексті є багато символів які потрібно відфільтрувати, щоб правильно порахувати кількості входжень. І числа також потрібно відфільтрувати*
+
+
+try:
+    with open('The_Everest_Story.txt') as file:
+        count_rows = 0
+        for line in file:
+            line
+            count_rows += 1
+        print(count_rows)  
+except IOError:
+    print("An IOError has occurred!")
