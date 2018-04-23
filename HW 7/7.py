6ab51e07faa6669952d70e21827e61ce1f87c94d
AlinaKovtun <alinakovtun11@gmail.com>
2018-04-23 15:23:34 +0300

    words count

--- HW 7/7.py
+++ HW 7/7.py
@@ -5,15 +5,60 @@
 #       - кількість голосних та приголосних букв
 #Зверніть увагу що в тексті є багато символів які потрібно відфільтрувати, щоб правильно порахувати кількості входжень. І числа також потрібно відфільтрувати*
 
-import re
 
+
+punctuation = ['.', ',', ':', ';', '!', '?', '(', ')']
 try:
     with open('The_Everest_Story.txt') as file:
         count_rows = 0
+        sstr = ''
         for line in file:
-            line
+            sstr += line
             count_rows += 1
         print('Number of lines in the file: ', count_rows)  
 except IOError:
     print('An IOError has occurred!')
-    print(len(re.findall(r'\w+','The_Everest_Story.txt')))
+    
+wordList = sstr.split()
+i = 0
+for word in wordList:
+    if word[-1] in punctuation:
+        wordList[i] = word[:-1]
+        word = wordList[i]
+    if word[0] in punctuation:
+        wordList[i] = word[1:]
+    i += 1
+    
+count_words = 0    
+i = 0
+while i < len(wordList):
+    i += 1
+    count_words += 1
+
+print('Number of words in the file: ', count_words)        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
