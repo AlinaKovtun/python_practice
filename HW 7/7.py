b4eea80116fdb82294f99a25867a2c085ec8d648
AlinaKovtun <alinakovtun11@gmail.com>
2018-04-23 20:28:45 +0300

    count vowels consonants

--- HW 7/7.py
+++ HW 7/7.py
@@ -5,9 +5,9 @@
 #       - кількість голосних та приголосних букв
 #Зверніть увагу що в тексті є багато символів які потрібно відфільтрувати, щоб правильно порахувати кількості входжень. І числа також потрібно відфільтрувати*
 
+import string
 
-
-punctuation = ['.', ',', ':', ';', '!', '?', '(', ')']
+punctuation = ['.', ',', ':', ';', '!', '?', '(', ')', '"', "'"]
 try:
     with open('The_Everest_Story.txt') as file:
         count_rows = 0
@@ -18,6 +18,8 @@ try:
         print('Number of lines in the file: ', count_rows)  
 except IOError:
     print('An IOError has occurred!')
+    
+    
     
 wordList = sstr.split()
 i = 0
@@ -32,10 +34,34 @@ for word in wordList:
 count_words = 0    
 i = 0
 while i < len(wordList):
+#    print(wordList[i], end=' ')
     i += 1
     count_words += 1
+    
+print('Number of words in the file: ', count_words)   
 
-print('Number of words in the file: ', count_words)        
+
+count_vowels = 0
+for i in sstr:
+    if i in list("AEIOUYaeiouy"):
+        count_vowels += 1    
+print('Number of vowels in the text: ', count_vowels)
+
+count_consonants = 0
+for i in sstr:
+    if i in list("BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz"):
+        count_consonants += 1    
+print('Number of consonants in the text: ', count_consonants)     
+
+
+
+
+
+
+
+
+
+
 
 
 
